12cb04999b1e30423c9201701222e44aedf4c726
dewilliam <790736503@qq.com>
2016-05-12 13:00:49 +0800

    Create bucket_sort.cpp
    桶排序

---
+++ bucket_sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void bucketsort(int* s,int length){
+	int max=s[0];
+	//find the maxmum value.....
+	for(int i=0;i<length;++i)
+		if(s[i]>max)
+			max=s[i];
+	//sort.....
+	int* buckets=new int[max+1];
+	for(int i=0;i<length;++i)
+		buckets[s[i]]++;
+	int j=0;
+	//copy back....
+	for(int i=0;i<max+1;++i)
+		if(buckets[i]>0)
+			for(int z=0;z<buckets[i];++z)
+				s[j++]=i;
+}
+int main(){
+	int s[]={5,3,9,12,4,7,22,0,11,6};
+	int length=sizeof(s)/sizeof(int);
+	bucketsort(s,length);
+	for(int i=0;i<length;++i)
+		cout<<s[i]<<",";
+	cout<<endl;
+}
