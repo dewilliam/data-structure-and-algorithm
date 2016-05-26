7e6c0fe3bc6b4fa7125cdfad541feb22f05a1ac4
dewilliam <790736503@qq.com>
2016-05-26 15:50:06 +0800

    Update 外部排序.cpp

--- 外部排序.cpp
+++ 外部排序.cpp
@@ -5,7 +5,7 @@
 //注意磁盘io的效率，这是该算法的主要时间消耗
 //空间消耗很少
 //author:谢添
-//location:北京交通大学 下一代互联网与互联工程国家工程实验室
+//location:北京交通大学 下一代互联网与互联设备国家工程实验室
 //email:mftianxie@gmail.com
 //time:16年5 月13
 #include <iostream>
